900d3159ff910f0f7762ee46f9add11053a4e8b1
LuizTh3 <luiz.gugu.ifac@gmail.com>
2026-02-24 16:23:34 -0500

    feat(Cards a página principal, Modelos de cards de colaborades e Carrossel): :sparkles: Cria novo template de página principal
    Adiciona Carrossel de promoções na página principal, e novos componentes visuais.

---
+++ assets/js/index/carrossel.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+        const track = document.getElementById('promo-carousel');
+        const slides = document.querySelectorAll('.carousel-slide');
+        const totalSlides = slides.length;
+
+        function moveSlide(direction) {
+            currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+            updateCarousel();
+        }
+
+        function updateCarousel() {
+            // Move a trilha para a esquerda baseado no índice atual
+            track.style.transform = `translateX(-${currentSlide * 100}%)`;
+        }
+
+        // Opcional: Faz o carrossel passar as fotos sozinho a cada 3 segundos
+        setInterval(() => {
+            moveSlide(1);
+        }, 3000);
